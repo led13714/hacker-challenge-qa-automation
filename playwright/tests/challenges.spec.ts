a3cb3e0cba30d5053ecedc348db677170780b1fd
David <david.lechler@intive.com>
2023-12-18 13:36:58 +0100

    reuse page and run tests sequentially (#6)

--- playwright/tests/challenges.spec.ts
+++ playwright/tests/challenges.spec.ts
@@ -1,5 +1,5 @@
-import { createWorker } from 'tesseract.js';
-import { test, expect } from "@playwright/test";
+import { createWorker } from "tesseract.js";
+import { test, expect, type Page } from "@playwright/test";
 import { StartPage } from "../pages/start-page";
 import { ChallengeOne } from "../pages/challenge-one-page";
 import { ChallengeTwo } from "../pages/challenge-two-page";
@@ -13,28 +13,44 @@ import { ChallengeNine } from "../pages/challenge-nine-page";
 import { ChallengeTen } from "../pages/challenge-ten-page";
 import { CompletePage } from "../pages/complete-page";
 
-test.describe("Hacker challenge speedrun", () => {
-  let startPage: StartPage;
-  let challengeOnePage: ChallengeOne;
-  let challengeTwoPage: ChallengeTwo;
-  let challengeThreePage: ChallengeThree;
-  let challengeFourPage: ChallengeFour;
-  let challengeFivePage: ChallengeFive;
-  let challengeSixPage: ChallengeSix;
-  let challengeSevenPage: ChallengeSeven;
-  let challengeEightPage: ChallengeEight;
-  let challengeNinePage: ChallengeNine;
-  let challengeTenPage: ChallengeTen;
-  let completePage: CompletePage;
-  let CHALLENGE_TEN_PASSWORD_URL = "http://localhost:8000/challenges/normal/jellyfish-password.json";
+let page: Page;
+let startPage: StartPage;
+let challengeOnePage: ChallengeOne;
+let challengeTwoPage: ChallengeTwo;
+let challengeThreePage: ChallengeThree;
+let challengeFourPage: ChallengeFour;
+let challengeFivePage: ChallengeFive;
+let challengeSixPage: ChallengeSix;
+let challengeSevenPage: ChallengeSeven;
+let challengeEightPage: ChallengeEight;
+let challengeNinePage: ChallengeNine;
+let challengeTenPage: ChallengeTen;
+let completePage: CompletePage;
+let CHALLENGE_TEN_PASSWORD_URL = "http://localhost:8000/challenges/normal/jellyfish-password.json";
 
-  test("First challenge", async ({ page }) => {
+// force sequential test execution (limited to 1 worker)
+test.describe.configure({ mode: 'serial' });
+
+test.describe("Hacker challenge speedrun", () => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
     startPage = new StartPage(page);
     challengeOnePage = new ChallengeOne(page);
     challengeTwoPage = new ChallengeTwo(page);
+    challengeThreePage = new ChallengeThree(page);
+    challengeFourPage = new ChallengeFour(page);
+    challengeFivePage = new ChallengeFive(page);
+    challengeSixPage = new ChallengeSix(page);
+    challengeSevenPage = new ChallengeSeven(page);
+    challengeEightPage = new ChallengeEight(page);
+    challengeNinePage = new ChallengeNine(page);
+    challengeTenPage = new ChallengeTen(page);
+    completePage = new CompletePage(page);
+  });
 
+  test("First challenge", async () => {
     await startPage.openPage();
-    await expect(page).toHaveTitle("Hacker Challenge");
+    await expect(startPage.heading).toBeVisible();
 
     await startPage.normalChallenge.click();
     await expect(challengeOnePage.heading).toBeVisible();
@@ -47,13 +63,7 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeTwoPage.heading).toBeVisible();
   });
 
-  test("Second challenge", async ({ page }) => {
-    challengeTwoPage = new ChallengeTwo(page);
-    challengeThreePage = new ChallengeThree(page);
-
-    await challengeTwoPage.openPage();
-    await expect(challengeTwoPage.heading).toBeVisible();
-
+  test("Second challenge", async () => {
     // enable and click button
     await challengeTwoPage.clickMe.evaluate((node) => node.removeAttribute("disabled"));
     await challengeTwoPage.clickMe.click();
@@ -61,31 +71,18 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeThreePage.heading).toBeVisible();
   });
 
-  test("Third challenge", async ({ page }) => {
-    challengeThreePage = new ChallengeThree(page);
-    challengeFourPage = new ChallengeFour(page);
-
-    await challengeThreePage.openPage();
-    await expect(challengeThreePage.heading).toBeVisible();
-
+  test("Third challenge", async () => {
     // double click
     await challengeThreePage.heading.dblclick();
 
     await expect(challengeFourPage.heading).toBeVisible();
   });
 
-  test("Fourth challenge", async ({ page }) => {
-    challengeFourPage = new ChallengeFour(page);
-    challengeFivePage = new ChallengeFive(page);
-
-    await challengeFourPage.openPage();
-    await expect(challengeFourPage.heading).toBeVisible();
-
+  test("Fourth challenge", async () => {
     // show and click link
     // dificulty: locator for hidden element & removal of css class
-    // // await challengeFourPage.invisibleLink.evaluate((node) => node.removeAttribute("hidden"));
     // solution: use pure JS!
-    const element = await page.$("a.button-style-link.hidden");
+    const element = await challengeFourPage.page.$("a.button-style-link.hidden");
     expect(element).toBeTruthy();
 
     // for the sake of inline compiler errors we have to check if element exists
@@ -104,13 +101,8 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeFivePage.heading).toBeVisible();
   });
 
-  test("Fiveth challenge", async ({ page }) => {
+  test("Fiveth challenge", async () => {
     let correctUrl: string;
-    challengeFivePage = new ChallengeFive(page);
-    challengeSixPage = new ChallengeSix(page);
-
-    await challengeFivePage.openPage();
-    await expect(challengeFivePage.heading).toBeVisible();
 
     // get name of next challenge from attribute
     let nextChallenge = await challengeFivePage.textButton.getAttribute("data-next-animal");
@@ -137,39 +129,27 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeSixPage.heading).toBeVisible();
   });
 
-  test("Sixth challenge", async ({ page }) => {
-    challengeSixPage = new ChallengeSix(page);
-    challengeSevenPage = new ChallengeSeven(page);
-
-    await challengeSixPage.openPage();
-    await expect(challengeSixPage.heading).toBeVisible();
-
+  test("Sixth challenge", async () => {
     //  get password from local storage
-    const localStorage = await page.evaluate(() => window.localStorage);
+    const localStorage = await challengeSixPage.page.evaluate(() => window.localStorage);
     const password = localStorage["password"];
-
+    
     // enter password and submit
     await challengeSixPage.passwordInput.fill(password);
     await challengeSixPage.submit.click();
-
+    
     await expect(challengeSevenPage.heading).toBeVisible();
   });
-
-  test("Seventh challenge", async ({ page }) => {
-    challengeSevenPage = new ChallengeSeven(page);
-    challengeEightPage = new ChallengeEight(page);
-
+  
+  test("Seventh challenge", async () => {
     // Get console log messages
     // https://stackoverflow.com/a/60075804
     let logMessage: any;
-    page.on("console", async (e) => {
+    challengeSevenPage.page.on("console", async (e) => {
       const args = await Promise.all(e.args().map((a) => a.jsonValue()));
       console.log(...args);
       logMessage = args;
     });
-
-    await challengeSevenPage.openPage();
-    await expect(challengeSevenPage.heading).toBeVisible();
 
     // click submit 3 times
     await challengeSevenPage.submit.dblclick();
@@ -183,13 +163,7 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeEightPage.heading).toBeVisible();
   });
 
-  test("Eigth challenge", async ({ page }) => {
-    challengeEightPage = new ChallengeEight(page);
-    challengeNinePage = new ChallengeNine(page);
-
-    await challengeEightPage.openPage();
-    await expect(challengeEightPage.heading).toBeVisible();
-
+  test("Eigth challenge", async () => {
     // remove blocker
     await challengeEightPage.blocker.evaluate((el) => {
       el.remove();
@@ -201,15 +175,10 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeNinePage.heading).toBeVisible();
   });
 
-  test("Nineth challenge", async ({ page }) => {
-    challengeNinePage = new ChallengeNine(page);
-    challengeTenPage = new ChallengeTen(page);
-    const worker = createWorker();
-
-    await (await worker).load();
-    await challengeNinePage.openPage();
-    await expect(challengeNinePage.heading).toBeVisible();
-
+  test("Nineth challenge", async () => {
+    const tesseractWorker = createWorker();
+    await (await tesseractWorker).load();
+    
     // reveal password image
     await challengeNinePage.passwordImage.evaluate((el) => {
       el.style.visibility = "visible";
@@ -217,19 +186,21 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeNinePage.passwordImage).toBeVisible();
 
     // take screenshot from website
-    await page.screenshot({ path: 'revealed_image_with_password.png' });
+    await challengeNinePage.page.screenshot({ path: "revealed_image_with_password.png" });
 
     // use Tesseract worker to recognize the text that is displayed on the website
     // https://github.com/tesseract-ocr/tesseract
-    const { data: { text } } = await (await worker).recognize('revealed_image_with_password.png');
-    console.log('### Recognized text on website:\n', text);
-    expect(text).not.toEqual('')
+    const {
+      data: { text },
+    } = await (await tesseractWorker).recognize("revealed_image_with_password.png");
+    console.log("### Recognized text on website:\n", text);
+    expect(text).not.toEqual("");
 
     // extract the password from the text
-    const extractedPassword = getStatedPasswordIn(text)
-    console.log('### Extracted password using OCR:\n', extractedPassword);
-    expect(extractedPassword).not.toEqual('')
-    
+    const extractedPassword = getStatedPasswordIn(text);
+    console.log("### Extracted password using OCR:\n", extractedPassword);
+    expect(extractedPassword).not.toEqual("");
+
     // fill in password and proceed
     await challengeNinePage.passwordInput.fill(extractedPassword);
     await challengeNinePage.submit.click();
@@ -237,24 +208,22 @@ test.describe("Hacker challenge speedrun", () => {
     await expect(challengeTenPage.heading).toBeVisible();
   });
 
-  test("Tenth challenge", async ({ page, request }) => {
-    challengeTenPage = new ChallengeTen(page);
-    completePage = new CompletePage(page);
-
-    await challengeTenPage.openPage();
-    await expect(challengeTenPage.heading).toBeVisible();
-
+  test("Tenth challenge", async ({ request }) => {
     // make api requests to get password -> 'jellyfish-password'
     const response = await request.get(CHALLENGE_TEN_PASSWORD_URL);
     expect(response.status()).toBe(200);
     const responseBody = await response.json();
-    const password = responseBody["password"]
+    const password = responseBody["password"];
 
     // fill in password and proceed
     await challengeTenPage.passwordInput.fill(password);
     await challengeTenPage.submit.click();
 
     await expect(completePage.congratulations).toBeVisible();
+  });
+
+  test.afterAll(async () => {
+    await page.close();
   });
 });
 
@@ -264,9 +233,9 @@ function getStatedPasswordIn(text: string) {
 
   if (match && match[1]) {
     const wordsAfterPassword = match[1].trim().split(/\s+/);
-    const resultString = wordsAfterPassword.join(' ');
+    const resultString = wordsAfterPassword.join(" ");
     return resultString;
   }
 
-  return '';
+  return "";
 }
